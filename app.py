43956cddd72758f0192827dfc0c950bb63ce33a7
Shirley Ding <122686936+pearjuicee@users.noreply.github.com>
2025-01-25 23:39:24 -0500

    Link routing and fix static files

--- app.py
+++ app.py
@@ -6,12 +6,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/camera')
+def camera():
+    return render_template('camera.html')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), 
                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/wardrobe')
+def wardrobe():
+    return render_template('wardrobe.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
